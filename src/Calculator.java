51e30c6eb0b1dcf1b485b61a65f789e9f797e4dd
junheeyeap <junheeyeap@gmail.com>
2021-07-21 18:11:10 +0900

    initial commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,8 @@
 
 public class Calculator {
+	
 	public static void main(String[] args) {
+		
 		System.out.println("µ¡¼À :" + ( 4 + 3 ) );
 		System.out.println("»¬¼À :" + ( 4 - 3 ) );
 		System.out.println("°ö¼À :" + ( 4 * 3 ) );
